a30dcd26b70b576636fb08dfb62154c3b35a8f5b
Aysnopye <babalolafestus28@gmail.com>
2023-03-18 16:02:22 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
  * maint - Prints numbers from 0 to 9.
  * Return: Always 0 (Success)
  */
-int main(void)
+
+int main(void);
 {
 	int i;
 
