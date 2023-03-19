b640272cfef60924abb3c6a8753af5cf475c753f
Aysnopye <babalolafestus28@gmail.com>
2023-03-18 18:12:52 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -5,27 +5,28 @@
  */
 int main(void)
 {
-	int n, m, 1;
-	for (n = 48 ; n < 58; n++)
+	int d, p, q;
+
+	for (d = '0'; d < '9'; d++)
 	{
-		for (n = 49; m < 58; m++)
-		{
-			for (1 = 50; 1 < 58; 1++)
-			{
-				if (1 > m && m > n)
-				{
-					putchar(n);
-					putchar(m);
-					putchar(1);
-					if (n != 55 || m != 56)
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
-			}
-		}
+	for (p = d + 1; p <= '9'; p++)
+	{
+	for (q = p + 1; q <= '9'; q++)
+	{
+	if ((p != d) != q)
+	{
+	putchar(d);
+	putchar(p);
+	putchar(q);
+	if (d == '7' && p == '8')
+	continue;
+	putchar(',');
+	putchar(' ');
 	}
+	}
+	}
+	}
+
 	putchar('\n');
 	return (0);
 }
