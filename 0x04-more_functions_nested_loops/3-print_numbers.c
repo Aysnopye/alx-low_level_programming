ed1b799364f371e4806546411d0deb114a95d7df
Aysnopye <babalolafestus28@gmail.com>
2023-03-23 17:32:50 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers -  checks for checks for a digit (0 through 9).
+ *
+ * Return: void.
+ */
+void print_numbers(void)
+{
+	char c;
+
+	for (c = 48; c < 58; ch++)
+	{
+		_putchar(c);
+	}
+		_putchar('\n');
+}
