038236b700fceaac949fa4006f9c1e87aa37860b
Aysnopye <babalolafestus28@gmail.com>
2023-03-28 14:03:47 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -23,7 +23,9 @@ int _atoi(char *s)
 	digit = 0;
 	while (s[len] != '\0')
 	len++;
+	{
 	while (i < len && f == 0)
+
 	}
 	if (s[i] == '-')
 	++d;
@@ -36,12 +38,13 @@ int _atoi(char *s)
 	f = 1;
 	if (s[i + 1] < '0' || s[i + 1] > '9')
 	break;
+	
 	f = 0;
 	}
 	i++;
-	}
+	{
 	if (f == 0)
-
+	}
 	return (0);
 
 	return (n);
